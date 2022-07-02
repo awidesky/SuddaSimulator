e6047ac829f1d8b69efdcf18e9f71bab508b667e
awidesky <awidesky@naver.com>
2022-07-02 20:33:31 +0900

    add Logger

---
+++ src/main/LoggerThread.java
@@ -0,0 +1,74 @@
+package main;
+
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.concurrent.LinkedBlockingQueue;
+
+
+public class LoggerThread extends Thread {
+
+	private PrintWriter logTo;
+	private LinkedBlockingQueue<Runnable> loggerQueue = new LinkedBlockingQueue<>();
+	
+	public volatile boolean isStop = false;
+	
+	public LoggerThread(OutputStream os) {
+		logTo = new PrintWriter(os);
+	}
+	
+	@Override
+	public void run() {
+
+		while (true) {
+
+			if (loggerQueue.isEmpty() && isStop) {
+				break;
+			}
+
+			try {
+				loggerQueue.take().run();
+			} catch (InterruptedException e) {
+				logTo.println("LoggerThread Interrupted! : " + e.getMessage());
+				logTo.println("Closing logger..");
+				break;
+			}
+		}
+		
+		logTo.close();
+
+	}
+	
+	public void log(String data) {
+
+		loggerQueue.offer(() -> {
+			logTo.println(data.replaceAll("\\R", System.lineSeparator()));
+		});
+		
+	}
+	
+	public void log(Exception e) {
+		
+		loggerQueue.offer(() -> {
+			e.printStackTrace(logTo);
+		});
+		
+	}
+	
+	/**
+	 * Kill LoggerThread in <code>timeOut</code> ms.
+	 * */
+	public void kill(int timeOut) {
+		
+		isStop = true;
+		
+		try {
+			this.join(timeOut);
+		} catch (InterruptedException e) {
+			logTo.println("Failed to join logger thread!");
+			e.printStackTrace(logTo);
+		}
+		
+		logTo.close();
+		
+	}
+}
